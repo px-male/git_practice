06ed59bdc4272cdba9d8018278e4b407150ad42c
px-male <caramel.bn@gmail.com>
2018-02-26 20:30:13 +0900

    test2ブランチで追加

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,9 +1,11 @@
-package practice;
+﻿package practice;
 
 public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+
+	System.out.println("test2ブランチ追加");	
 
 	}
 
