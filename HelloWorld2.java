654e4bcc0e7a45346d62e6333c786df63af578ba
px-male <caramel.bn@gmail.com>
2018-03-01 19:57:54 +0900

    comit

--- HelloWorld2.java
+++ HelloWorld2.java
@@ -4,8 +4,8 @@ public class HelloWorld2 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
-		System.out.println("test");
+		//bugfix
+		System.out.println("test11");
 	}
 
 }
